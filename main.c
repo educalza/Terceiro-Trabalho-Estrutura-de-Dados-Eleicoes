43de37c579d0cca6186c2de43232c0103a14b409
Eduardo <eduardoalvescalza20@gmail.com>
2024-09-28 16:52:26 -0300

    Autoria e descricao do projeto

--- main.c
+++ main.c
@@ -1,3 +1,19 @@
+/*
+ * Autor: Eduardo Alves Calza
+ * GitHub: https://github.com/educalza
+ * Data: 28 de setembro de 2024
+
+ * Descrição: Este projeto simula uma eleição para o cargo de prefeito utilizando a linguagem C.
+ * O programa permite o cadastro de chapas com candidatos e seus vices, além de realizar a votação,
+ * contabilizando votos válidos, brancos e nulos. 
+ * Ele também implementa a lógica de dois turnos de acordo com as regras eleitorais,
+ * incluindo o critério de desempate pela idade dos candidatos. 
+ * O boletim de urna é salvo em um arquivo após cada turno, apresentando as estatísticas detalhadas da eleição. 
+ * O foco do projeto está no uso adequado de estruturas de dados, com análise da complexidade temporal de cada função no relatório final.
+ * A estrutura de dados escolhida para realizar o projeto foi a Lista dinâmica, por ser mais fácil e rápida de usar.
+ */
+
+
 #include <stdio.h>
 #include "eleicoes.h"
 
