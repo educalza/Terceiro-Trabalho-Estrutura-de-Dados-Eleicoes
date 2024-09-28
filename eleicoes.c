71eaff9c2559f4def0159457cfed19785cdee1df
Eduardo <eduardoalvescalza20@gmail.com>
2024-09-28 00:35:06 -0300

    mudanca eleicoes.c

--- eleicoes.c
+++ eleicoes.c
@@ -11,6 +11,8 @@ typedef struct chapa{
     int DataDeNascimento[3];
 
     char nomeVice[NOME];
+
+    int votacao;
 } Chapa;
 
 typedef struct lista{
@@ -20,17 +22,8 @@ typedef struct lista{
 
 void simulaUrnaVotacao(){
 
-    Chapa *C = (Chapa*)malloc(sizeof(Chapa));
-    if(C == NULL){
-        printf("Erro de alocacao.\n");
-        exit(1);
-    }
-
-    Lista *lst = (Lista*)malloc(sizeof(Lista));
-    if(lst == NULL){
-        printf("Erro de alocacao.\n");
-        exit(1);
-    }
+    Chapa *C = NULL, *chapaAux;
+    Lista *lst = NULL;
 
     FILE *fp_boletimPrimeiroTurno = fopen("boletim.txt", "w");
     if(fp_boletimPrimeiroTurno == NULL) exit(1);
@@ -46,42 +39,62 @@ void simulaUrnaVotacao(){
     system("pause"); system("cls");
 
 
+
     printf("---------- Cadastro de chapas ----------\n");
     printf("Sistema de parada = 0\n\n");
-    C = cadastrarChapas(nChapas);
-    lst = insereChapaLista(C, lst);
-
-
-}
-
-Chapa *cadastrarChapas(int *nChapas){
-    Chapa *novo = (Chapa*)malloc(sizeof(Chapa));
-    if(novo == NULL) exit(1);
-
     int count = 0;
     while(count < nChapas){
-
         printf("Prefeito\n");
 
         printf("Numero: ");
-        scanf("%d", &novo->numero);
-        if(novo->numero == 0){
+        scanf("%d", &chapaAux->numero);
+        if(chapaAux->numero == 0){
             printf("Cadastro de chapa cancelado.\n");
             exit(0);
         }
-        
+
         printf("Nome: ");
         fflush(stdin);
-        fgets(novo->nomePrefeito, NOME, stdin);
+        fgets(chapaAux->nomePrefeito, NOME, stdin);
 
         printf("\nData de nascimento dia/mes/ano");
         for(int i=0; i<3; i++){
-            scanf("%d%%d%d", novo->DataDeNascimento[0], novo->DataDeNascimento[1], novo->DataDeNascimento[2]);
+            scanf("%d%%d%d", chapaAux->DataDeNascimento[0], chapaAux->DataDeNascimento[1], chapaAux->DataDeNascimento[2]);
         }
-        return novo;
+        system("pause");
         system("cls");
+        C = cadastrarChapas(chapaAux->nomePrefeito, chapaAux->numero, chapaAux->DataDeNascimento, chapaAux->nomeVice);
+        lst = insereChapaLista(C, lst);
     }
-    return 0;
+    printf("\n-----------------------------------------\n");
+
+    printf("---------- Votacao das Chapas ----------\n");
+    printf("Candidatos... \n\n");
+    imprimeCandidatosLista(lst);
+    printf("\nDigite sua votacao: ");
+    int *votacao;
+    scanf("%d", &votacao);
+
+}
+
+Chapa *cadastrarChapas(char *nomePrefeito, int numero, int *data_nascimento, char *nomeVice){
+
+    Chapa *novo = (Chapa*)malloc(sizeof(Chapa));
+    if(novo == NULL) exit(1);
+
+    strncpy(novo->nomePrefeito, nomePrefeito, NOME - 1);
+    novo->nomePrefeito[NOME-1] = '\0';
+
+    novo->numero = numero;
+    for(int i=0; i<3; i++){
+        novo->DataDeNascimento[i] = data_nascimento[i];
+    }
+    strncpy(novo->nomeVice, nomeVice, NOME - 1);
+    novo->nomeVice[NOME - 1] = '\0';
+
+    novo->votacao = 0;
+
+    return novo;
 }
 
 Lista *insereChapaLista(Chapa *C, Lista *lst){
@@ -93,4 +106,26 @@ Lista *insereChapaLista(Chapa *C, Lista *lst){
     novo->prox = lst;
     fprinf("%s %d\n%s\n\n", novo->C->nomePrefeito, novo->C->numero, novo->C->nomeVice);
     return novo;
+}
+
+
+void imprimeCandidatosLista(Lista* lst){
+    for(Lista *p = lst; p != NULL; p = p->prox){
+        printf("Prefeito %s %d\nVice %s\n\n", lst->C->nomePrefeito, lst->C->numero, lst->C->nomeVice);
+    }
+}
+
+Lista *votarCandidatoDaChapa(int *votacao, Lista *lst, int *qntChapa){
+
+    Lista *aux = NULL;
+    for(int i = 0; i<qntChapa; i++){
+        int count = 0;
+        for(Lista *p = lst; p=!NULL; p=p->prox){
+            if(votacao == p->C->numero){
+                aux = p;
+                break;
+            }
+        }
+    }
+    return aux->C->votacao;
 }
